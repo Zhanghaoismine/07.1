e4531c1022367a38ca47f28f142655d8ac56c40c
Zhanghaoismine <kegberry@naver.com>
2023-10-19 16:03:26 +0900

    4

--- main.c
+++ main.c
@@ -3,16 +3,18 @@
 
 void f(void);
 
-int i;
 int main(void)
 {
+	int i;
 	for(i=0; i<5;i++)
 	{
 		f();
 	}
+	system("PAUSE");
 	return 0;
 }
 void f(void){
+	int i;
 	for (i=0;i<10;i++)
 	printf("#");
 }
