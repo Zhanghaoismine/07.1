83aee0c15cc25a35f74f193770dfc3228901b2c8
Zhanghaoismine <kegberry@naver.com>
2023-10-19 15:46:57 +0900

    3

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int inc(int counter);
+int main(void) {
+	int i=10;
+	printf("함수 호출 전 i=%d\n", i);
+	i=inc(i);
+	printf("함수 호출 후 i=%d\n",i);
+	
+	system("PAUSE");
+	return 0;
+}
+int inc(int counter)
+{
+	counter++;
+	return counter;
+}
+
+
+
